66d91043e6ace7ad4fe92374e556e09c3f50364d
Oleh <olehfromukraine@gmail.com>
2021-03-31 20:40:30 +0300

    fix someplace

--- src/main/java/singleton/simple/TestSimpleSingleton.java
+++ src/main/java/singleton/simple/TestSimpleSingleton.java
@@ -2,7 +2,6 @@ package main.java.singleton.simple;
 
 public class TestSimpleSingleton {
     public static void main(String[] args) {
-
         Person firstPerson = new Person("Oleh", 91);
         Person secondPerson = new Person("Riko", 32);
         Singleton firstSingleton = Singleton.getInstance(firstPerson);
